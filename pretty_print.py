a4696072d138807ab22487412b70e01eb8a8db64
richardleung1 <richardl57549@gmail.com>
2021-01-09 02:55:40 -0500

    [pretty_print] Complete function

--- pretty_print.py
+++ pretty_print.py
@@ -1,5 +1,5 @@
 # You will have to figure out what parameters to include
-# 🚨 All functions must use recursion 🚨
+# All functions must use recursion 
 
 # Write a function that accepts a complex dictionary and prints out all of it's keys and all of its values. 
 # The dictionary can have dictionaries nested inside of it
@@ -11,16 +11,23 @@
 
 def pretty_print(dictionary, indent):
     # Write code here
-    pass
+    for key in dictionary:
+        value = dictionary[key]
+        if type(value) is dict:
+            print (f"{indent}{key}:")
+            pretty_print(value, indent + indent)
+        else:
+            print (f"{indent}{key}: {value}")
 
 o1 = {"a": 1, "b": 2}
 o2 = {"a": 1, "b": 2, "c": {"name": "Bruce Wayne", "occupation": "Hero"}, "d": 4}
 o3 = {"a": 1, "b": 2, "c": {"name": "Bruce Wayne", "occupation": "Hero", "friends": {"spiderman": {"name": "Peter Parker"}, "superman": {"name": "Clark Kent"}}}, "d": 4}
 
-# print(pretty_print(o1, "-"))
-# print(pretty_print(o2, " "))
-# print(pretty_print(o3, ".."))
-# ..a: 1
+# pretty_print(o1, "-")
+# pretty_print(o2, " ")
+pretty_print(o3, "..")
+
+# ..a: 1 
 # ..b: 2
 # ..c:
 # ....name: Bruce Wayne
