aead8d75c98ac05b09a290a6132bedd484a7df4c
richardleung1 <richardl57549@gmail.com>
2021-01-09 01:04:23 -0500

    [find_max] Complete function

--- find_max.py
+++ find_max.py
@@ -5,7 +5,17 @@
 
 def find_max(l):
     # Write code here
-    pass
+    if len(l) <= 0:
+        return 'Array cannot be empty'
+    if len(l) == 1:
+        return l[0]
+    else:
+        max = find_max(l[1:])
+        if max > l[0]:
+            return max
+        else:
+            return l[0]
 
-# print(find_max([1, 4, 45, 6, -50, 10, 2]))
+
+print(find_max([1, 4, 45, 6, -50, 10, 2]))
 # => 45
